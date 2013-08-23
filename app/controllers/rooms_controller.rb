7491272acafdb6bd957e2f9874e83ec2497bac2b
spoudyal <subashpoudyal@lftechnology.com>
2013-08-23 10:20:03 +0545

    devise authentication and rooms controller added

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,13 @@
+class RoomsController < ApplicationController
+  def index
+  end
+
+  def add
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Mrrs::Application.routes.draw do
+  # get "rooms/add"
+  # get "rooms/edit"
+  # get "rooms/delete"
+  # get "rooms/index"
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :rooms
+  # root 'rooms#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
