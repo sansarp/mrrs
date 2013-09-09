0fd498063e8f02c810de6947b2d9549669114611
spoudyal <subashpoudyal@lftechnology.com>
2013-09-09 09:39:55 +0545

    open auth @lftechnology validation

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,17 +4,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
 	    # binding.pry
 	    checkmail = @user.email
-    	if checkmail.present? and not checkmail.match( /\A([^@\s]+)@lftechnology.com\Z/) ==false
+    	if checkmail.present? and not checkmail.match( /\A([^@\s]+)@lftechnology.com\Z/) 
      		checkmail = false
+     	else
+     		checkmail = true
      	end
-     	 # binding.pry
-	    if @user.persisted? and checkmail == true
+     	    # binding.pry
+	    if @user.persisted? and checkmail 
 	      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
 	      sign_in_and_redirect @user, :event => :authentication
-	      # binding.pry
 	    else
-	      session["devise.google_data"] = request.env["omniauth.auth"]
-	      redirect_to root_path and return
+	       session["devise.google_data"] = request.env["omniauth.auth"]
+	       flash[:error] = I18n.t 'devise.omniauth_callbacks.failure', 
+	            :kind => 'Google', :reason => 'only @lftechnology domain is allowed'
+	      redirect_to root_path 
+	      # binding.pry
 	    end
 	end
 end
