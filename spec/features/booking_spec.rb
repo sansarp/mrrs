38dd4af9afe6d322b58049d8c3e9c0c2bc88c15c
spoudyal <subashpoudyal@lftechnology.com>
2013-09-09 16:39:42 +0545

    test spec enhancement

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -1,15 +1,44 @@
 require_relative '../spec_helper'
 
-describe "Booking is done for" do
+describe "Booking" do
 	before(:each) do 
 		@user = User.create!(
 			email: 'connect.subash@gmail.com',
 			password: 'password'
 			)
 	end
-it "should  allow room add for authenticated user" do
+it "should  allow room booking for authenticated user" do
 	login_as @user  
-	@room = Room.new
-	visit new_room_path 
-end
+	@booking = Booking.new
+	visit  new_booking_path  
+
+	fill_in 'Title', with: 'ROR meetup'
+	fill_in 'Description', with: 'weekly meeting'
+	
+	select("2013", :from => "booking_start_time_1i")
+	select("October", :from => "booking_start_time_2i")
+	select("10", :from => "booking_start_time_3i")
+	select("09", :from => "booking_start_time_4i")
+	select("02", :from => "booking_start_time_5i")
+
+	select("2013", :from => "booking_end_time_1i")
+	select("October", :from => "booking_end_time_2i")
+	select("10", :from => "booking_end_time_3i")
+	select("10", :from => "booking_end_time_4i")
+	select("02", :from => "booking_end_time_5i")
+	# select 'October' from => 'booking_start_time_2i'
+	# select '10' :from => 'booking_start_time_3i'
+	# select '09' :from => 'booking_start_time_4i'
+	# select '02' :from => 'booking_start_time_5i'
+
+	# select '2013' :from => 'booking_end_time_1i'
+	# select 'October' :from => 'booking_end_time_2i'
+	# select '11' :from => 'booking_end_time_3i'
+	# select '09' :from => 'booking_end_time_4i'
+	# select '02' :from => 'booking_end_time_5i'
+	
+	click_button 'Save'
+	expect(page).to have_content 'Room booked !!!'
+	save_and_open_page
+	end
 end
--- spec/features/room_creation_spec.rb
+++ spec/features/room_creation_spec.rb
@@ -1,26 +1,24 @@
 require_relative '../spec_helper'
 
 describe "Room creation" do
-	# before(:each) do 
-	# 	@user = User.create!(
-	# 		email: 'connect.subash@gmail.com',
-	# 		password: 'password'
-	# 		)
-	# end
-
-	it "only admin user can create room" do
-		# login_as @user  
-		visit '/rooms/new' 
-		expect(page).to have_content 'Name'
-		# print page.html
-		# fill_in 'Name', with: 'Room 1'
-		# fill_in 'Description', with: 'vip meeting room'
-		# fill_in 'Capacity', with: '8'
-		# fill_in 'Status', with: '1'
-		# click_button 'Save'
-		# expect(page).to have_content 'Room was successfully created.'
-		# save_and_open_page
-		print page.html
+	before(:each) do 
+		@user = User.create!(
+			email: 'connect.subash@gmail.com',
+			password: 'password'
+			)
 	end
+it "should  allow room add for authenticated user" do
+	login_as @user  
+	@room = Room.new
+	visit new_room_path 
 
+	fill_in 'Name', with: 'Room 1'
+	fill_in 'Description', with: 'vip meeting room'
+	fill_in 'Capacity', with: '8'
+	# fill_in checkbox 'Status', with: '1'
+	choose('room_status_true')
+	click_button 'Save'
+	expect(page).to have_content 'Room was successfully created.'
+	save_and_open_page
+	end
 end
