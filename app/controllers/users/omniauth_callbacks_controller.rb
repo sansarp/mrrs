9093f4dba1df8ad13076395be810317561ecad70
spoudyal <subashpoudyal@lftechnology.com>
2013-09-09 10:08:48 +0545

    remove permission access step in omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
 	      sign_in_and_redirect @user, :event => :authentication
 	    else
-	       session["devise.google_data"] = request.env["omniauth.auth"]
+	       # session["devise.google_data"] = request.env["omniauth.auth"]
 	       flash[:error] = I18n.t 'devise.omniauth_callbacks.failure', 
 	            :kind => 'Google', :reason => 'only @lftechnology domain is allowed'
 	      redirect_to root_path 
